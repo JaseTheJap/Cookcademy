a1be378024b8754e5aa86b98f1f4b16a42c0d43e
JaseTheJap <ammamusicschool@gmail.com>
2023-05-10 11:17:40 +0200

    Initial Commit

---
+++ Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Jason Hattingh on 2023/05/10.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
